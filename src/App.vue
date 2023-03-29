9939c036f5eb57b2e476312369ce57a43485bed1
mhdrmaulana <129238875+mhdrmaulana@users.noreply.github.com>
2023-03-30 00:57:20 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,85 @@
+<script setup>
+</script>
+
+<template>
+    <h1><center>Clasher Indonesia</center></h1>
+    <center><img src="https://o.remove.bg/downloads/7c09d4c1-6404-4956-82bf-15c1fca2d806/lavacoc-removebg-preview.png" alt="Logo Games" style="width:180px;height:200px;"></center>
+    
+    <h2><center>Login</center></h2>
+ <form action="/action_page.php"><center>
+        <label for="usname">Nickname:</label>
+        <input type="text" id="usname" name="usname"><br><br>
+        <label for="password">Password:</label>
+        <input type="text" id="Password" name="Password"><br><br>
+        <input type="submit" value="Lets Go"></center>
+        <p>Clash of Clans adalah permainan video strategi seluler freemium yang dikembangkan dan diterbitkan oleh pengembang permainan Finlandia, Supercell. Permainan ini dirilis untuk platform iOS pada 2 Agustus 2012, dan di Google Play untuk Android pada 7 Oktober 2013. Permainan ini berlatarkan tema fantasi[3] dunia persistent[4] dimana pemainnya adalah kepala desa. Clash of Clans menugaskan pemain untuk membangun desa mereka sendiri menggunakan sumber daya yang diperoleh dari menyerang desa pemain lain atau memproduksinya di desa sendiri. Untuk menyerang, pemain melatih berbagai jenis pasukan menggunakan sumber daya. Sumber daya utamanya adalah emas, elixir dan elixir hitam. Pemain dapat bergabung untuk membuat klan, grup hingga lima puluh orang, yang kemudian dapat berpartisipasi dalam Perang Klan bersama, menyumbang dan menerima pasukan, dan mengobrol satu sama lain.</p>
+       
+        <form action="/action_page.php"><label for="drink">Pilih Town Hall:</label>
+  <select name="drink" id="drink">
+    <option value="Town Hall 15">Town Hall 15</option>
+    <option value="Town Hall 14">Town Hall 14</option>
+    <option value="Town Hall 13">Town Hall 13</option>
+    <option value="Town Hall 12">Town Hall 12</option>
+    <option value="Town Hall 11">Town Hall 11</option>
+    <option value="Town Hall 10">Town Hall 10</option>
+    <option value="Town Hall 9">Town Hall 9</option>
+    <option value="Town Hall 8">Town Hall 8</option>
+    <option value="Town Hall 7">Town Hall 7</option>
+    <option value="Town Hall 6">Town Hall 6</option>
+    <option value="Town Hall 5">Town Hall 5</option>
+    <option value="Town Hall 4">Town Hall 4</option>
+    <option value="Town Hall 3">Town Hall 3</option>
+  </select>
+  <br><br>
+  </form>
+</form>
+<form action="/action_page.php">
+        <p><label for="Cofee Shop">Jika ada saran atau masukan comment dibawah ini:</label></p>
+        <textarea id="w3review" name="w3review" rows="4" cols="50">Kritik Dan Saran</textarea>
+        <br>
+    </form>   
+
+    <table>
+  <tr>
+    <th>Admin 1</th>
+    <th>Admin 2</th>
+    <th>Admin 3</th>
+  </tr>
+  <tr>
+    <td>Manager</td>
+    <td>Capten</td>
+    <td>Staff</td>
+  </tr>
+  <tr>
+    <td>Jhon</td>
+    <td>Ryuu</td>
+    <td>Haqqqi</td>
+  </tr>
+</table>
+    <h4><center>SALAM CLASH ON</center></h4>
+<h5><center>Clash of Clans</center></h5>
+<h6><center>Indonesia
+  
+</center></h6>
+<a href="https://id.wikipedia.org/wiki/Clash_of_Clans">KLIK JIKA MASIH BINGUNG</a>
+</template>
+
+<style scoped>
+  
+
+</style>
+<style>
+a {color:black}
+h1 {color: aliceblue;}
+h3 {color: aliceblue;}
+h4 {color: aliceblue;}
+h5 {color: aliceblue;}
+h6 {color: aliceblue;}
+body {
+    background-image: url("https://www.komputermedia.com/wp-content/uploads/2018/10/Background-Merah-HD.png");
+  background-size: cover;}
+  table, th, td {border:2px solid #f8f3f3; color: aliceblue;} 
+  form,p {color: aliceblue; margin-top: 5%;}
+  h2 {color: aliceblue;}
+  
+</style>
